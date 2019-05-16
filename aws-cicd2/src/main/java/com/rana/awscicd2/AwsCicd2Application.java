0b2925665f4f3ff6299d2731979873494a78feb7
ibm_admin <ibm_admin@9.77.94.2>
2019-05-16 22:49:03 +0530

    good luck

---
+++ aws-cicd2/src/main/java/com/rana/awscicd2/AwsCicd2Application.java
@@ -0,0 +1,13 @@
+package com.rana.awscicd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCicd2Application.class, args);
+	}
+
+}
